94a1d310662c41d98330b5823872ac2243a296a2
Kyrylo Yakymenko <yakimych@gmail.com>
2018-12-15 18:21:30 +0100

    Parse stepSize to int and use on Prev/Next

--- src/Main.elm
+++ src/Main.elm
@@ -1,9 +1,9 @@
 module Main exposing (main)
 
 import Browser
-import Html exposing (Attribute, Html, button, div, input, text)
+import Html exposing (Attribute, Html, button, div, input, span, text)
 import Html.Attributes exposing (..)
-import Html.Events exposing (onInput)
+import Html.Events exposing (onClick, onInput)
 
 
 main =
@@ -17,13 +17,14 @@ main =
 
 type alias Model =
     { stepSizeString : String
+    , stepSize : Int
     , currentIteration : Int
     }
 
 
 init : () -> ( Model, Cmd Msg )
 init _ =
-    ( { stepSizeString = "1", currentIteration = 0 }, Cmd.none )
+    ( { stepSizeString = "1", stepSize = 1, currentIteration = 0 }, Cmd.none )
 
 
 type Msg
@@ -36,20 +37,32 @@ update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
         Next ->
-            ( { model | currentIteration = model.currentIteration + 1 }, Cmd.none )
+            ( { model | currentIteration = model.currentIteration + model.stepSize }, Cmd.none )
 
         Previous ->
-            ( { model | currentIteration = model.currentIteration - 1 }, Cmd.none )
+            ( { model | currentIteration = model.currentIteration - model.stepSize }, Cmd.none )
 
         ChangeStepSize newStepSizeString ->
-            ( { model | stepSizeString = newStepSizeString }, Cmd.none )
+            let
+                parsedStepSize =
+                    String.toInt newStepSizeString
+            in
+            ( { model
+                | stepSizeString = newStepSizeString
+                , stepSize = String.toInt newStepSizeString |> Maybe.withDefault model.stepSize
+              }
+            , Cmd.none
+            )
 
 
 view : Model -> Html Msg
 view model =
     div []
         [ input [ value model.stepSizeString, onInput ChangeStepSize ] []
-        , div [] [ text "Test" ]
+        , span [] [ text (String.fromInt model.stepSize) ]
+        , div [] [ text ("Current iteration: " ++ String.fromInt model.currentIteration) ]
+        , button [ onClick Previous ] [ text "Prev" ]
+        , button [ onClick Next ] [ text "Next" ]
         ]
 
 
