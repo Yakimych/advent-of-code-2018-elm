e1ea74289e912eb11510c87297935c49d6ae8c11
Kyrylo Yakymenko <yakimych@Kyrylos-MBP.lan>
2018-12-22 20:45:08 +0100

    Refactor away redundant code

--- src/Day08Logic.elm
+++ src/Day08Logic.elm
@@ -1,37 +1,13 @@
 module Day08Logic exposing (calculateSum)
 
-import Debug exposing (..)
-
 
 calculateSum : List Int -> Int
 calculateSum numbers =
-    let
-        nodes =
-            numbers |> List.head |> Maybe.withDefault 0
-
-        items =
-            numbers |> List.drop 1 |> List.head |> Maybe.withDefault 0
-
-        rest =
-            numbers |> List.drop 2
-    in
-    case nodes of
-        0 ->
-            rest |> List.take items |> List.sum
-
-        _ ->
-            let
-                ( sum, toClip ) =
-                    getSumAndNumberOfElements nodes items rest
-
-                newList =
-                    nodes - 1 :: items :: (rest |> List.drop toClip)
-            in
-            sum + calculateSum newList
+    getSumAndElementsUsed 1 0 numbers |> Tuple.first
 
 
-getSumAndNumberOfElements : Int -> Int -> List Int -> ( Int, Int )
-getSumAndNumberOfElements nodes items elements =
+getSumAndElementsUsed : Int -> Int -> List Int -> ( Int, Int )
+getSumAndElementsUsed nodes items elements =
     case nodes of
         0 ->
             ( elements |> List.take items |> List.sum, items )
@@ -44,13 +20,13 @@ getSumAndNumberOfElements nodes items elements =
                 newItems =
                     elements |> List.drop 1 |> List.head |> Maybe.withDefault 0
 
-                newRest =
+                newElements =
                     elements |> List.drop 2
 
                 ( sum, toClip ) =
-                    getSumAndNumberOfElements newNodes newItems newRest
+                    getSumAndElementsUsed newNodes newItems newElements
 
                 ( latestSum, latestToClip ) =
-                    getSumAndNumberOfElements (x - 1) items (elements |> List.drop (toClip + 2))
+                    getSumAndElementsUsed (x - 1) items (elements |> List.drop (toClip + 2))
             in
             ( sum + latestSum, latestToClip + toClip + 2 )
